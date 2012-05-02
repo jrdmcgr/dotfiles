37933d19f1a92b57fac8a10c8a736e64dfa88e41
Jared McGuire <jrdmcgr@gmail.com>
2012-05-01 22:17:55 -0400

    Changed git completion PS1.

--- Global/PS1.sh
+++ Global/PS1.sh
@@ -2,5 +2,5 @@ PS1='〈 \w  〉\[\e[0;32m\]⚡\[\e[0m\]  '
 
 if [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]; then
 	. /usr/local/etc/bash_completion.d/git-completion.bash;
-	PS1='〈 \w  〉$(__git_ps1 "%s") \n \[\e[0;32m\]⚡\[\e[0m\]  '
+	PS1='〈 \w$(__git_ps1 " ± %s") 〉\[\e[0;32m\]⚡\[\e[0m\]  '
 fi
