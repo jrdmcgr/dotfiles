bf145808e1fc529ba0a7bc83d400b71d5044bd40
Jared McGuire <jrdmcgr@gmail.com>
2012-05-03 19:24:41 -0400

    sublr: quickly edit a remote file with Sublime Text.

--- Darwin/alias.sh
+++ Darwin/alias.sh
@@ -1,2 +1,9 @@
 alias vbox='VBoxManage'
 alias ll='ls -laFG'
+
+function sublr {
+	filename=$(echo $1 | egrep -o '[a-zA-Z\.\-]*$') &&
+	scp $1 ./ && 
+	subl -w ./$filename &&
+	scp ./$filename $1
+}
