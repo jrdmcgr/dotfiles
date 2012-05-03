dd8534b5fff420467750312d36af7d4238eb9bad
Jared McGuire <jrdmcgr@gmail.com>
2012-05-03 12:04:04 -0400

    Added rebash and tt aliases.

--- Global/alias.sh
+++ Global/alias.sh
@@ -1,3 +1,9 @@
+# Reload .bashrc
+alias rebash='. ~/.bashrc'
+
 alias tailf="tail -f"
 alias +x="chmod +x $1"
-alias tabtitle='echo -e "\033];${1}\007"'
+
+# Set the tab title in iTerm 2
+function tt { echo -ne "\033];${1}\007"; }
+
