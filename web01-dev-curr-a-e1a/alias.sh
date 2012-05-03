1ec7ae964ccbbcf63508de3ec9799fc2e101a2eb
Jared McGuire <jrdmcgr@gmail.com>
2012-05-03 12:05:42 -0400

    Added tablediff function for Curriculog host.

--- web01-dev-curr-a-e1a/alias.sh
+++ web01-dev-curr-a-e1a/alias.sh
@@ -1,5 +1,7 @@
 echo "Loaded curriculog aliases."
 function tablegrep { mysql jmcguire -e "show tables" | egrep ${1}; }
 function trunctablegrep { for i in `tablegrep ${1}`; do mysql jmcguire -e "truncate table $i"; done; }
+function tablediff { diff -u <(mysql ${1} -e 'show tables') <(mysql ${2} -e 'show tables') | vi -; }
+
 alias mysqle='mysql jmcguire -e'
 alias pykill='pkill -u jmcguire python'
