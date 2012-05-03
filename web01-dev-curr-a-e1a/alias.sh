1f3a5a6261996edb1583c03d5ea07cf5416974f9
Jared McGuire <jrdmcgr@gmail.com>
2012-05-03 12:35:56 -0400

    Fixed syntax errors with mysql functions.

--- web01-dev-curr-a-e1a/alias.sh
+++ web01-dev-curr-a-e1a/alias.sh
@@ -2,6 +2,6 @@ alias mysqle='mysql jmcguire -e'
 alias pykill='pkill -u jmcguire python'
 
 ## MySQL data wrangling functions.
-tablegrep() mysql jmcguire -e "show tables" | egrep ${1};
-trunctablegrep() for i in `tablegrep ${1}`; do mysql jmcguire -e "truncate table $i"; done;
-tablediff() diff -u <(mysql ${1} -e 'show tables') <(mysql ${2} -e 'show tables') | vi -;
+tablegrep() { mysql jmcguire -e "show tables" | egrep ${1}; }
+trunctablegrep() { for i in `tablegrep ${1}`; do mysql jmcguire -e "truncate table $i"; done; }
+tablediff() { diff -u <(mysql ${1} -e 'show tables') <(mysql ${2} -e 'show tables') | vi -; }
