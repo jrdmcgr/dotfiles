debc196f293b0712f7b3cce1711a634175d319fe
Jared McGuire <jrdmcgr@gmail.com>
2012-06-08 08:38:52 -0400

    More PS1 fixes.

--- Global/PS1.sh
+++ Global/PS1.sh
@@ -1,7 +1,7 @@
 home_prompt() {
 	# If we're home then use a simple prompt.
 	if [ `pwd` == $HOME ]; then 
-		PS1=' \[\e[0;32m\]⚡ \[\e[0m\] '
+		PS1=' \[\e[0;32m\]⚡\[\e[0m\] '
 	# Check for git bash completion on Mac.
 	elif [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]; then
 		. /usr/local/etc/bash_completion.d/git-completion.bash;
@@ -9,7 +9,7 @@ home_prompt() {
 	# Check for git bash completion on Linux.	
 	elif [ -f /etc/bash_completion.d/git ]; then
 		. /etc/bash_completion.d/git;
-		PS1='〈 \w$(__git_ps1 " ±%s") 〉\[\e[0;32m\]⚡\[\e[0m\]  '
+		PS1='〈 \w$(__git_ps1 " ± %s") 〉\[\e[0;32m\]⚡\[\e[0m\]  '
 	# Display the standard prompt.
 	else
 		PS1='〈 \w 〉\[\e[0;32m\]⚡\[\e[0m\]  '
