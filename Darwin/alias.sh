ba236b562ef93e644e1644821fd79d5981b257d4
Jared McGuire <jrdmcgr@gmail.com>
2012-06-07 20:37:39 -0400

    Added myip function for Darwin hosts which parses ifconfig to tell me my lan ip.
    Small adjustment to Darwin alias.

--- Darwin/alias.sh
+++ Darwin/alias.sh
@@ -1,9 +1,19 @@
 alias vbox='VBoxManage'
 alias ll='ls -laFG'
 
-function sublr {
+# Edit a remote file with Sublime Text.
+sublr() {
 	filename=$(echo $1 | egrep -o '[a-zA-Z\.\-]*$') &&
 	scp $1 ./ && 
 	subl -w ./$filename &&
 	scp ./$filename $1
 }
+
+# Print this machine's LAN IP and copy it to the clipboard.
+myip() { 
+	IP=$(ifconfig | awk '/inet 192/ { print $2 }')
+	echo $IP
+	echo -n $IP | pbcopy
+}
+
+
