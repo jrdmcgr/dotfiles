36b9e00027956e3d4c0987e699c4768d83112406
Jared McGuire <jrdmcgr@gmail.com>
2012-06-07 22:46:45 -0400

    Added a random string generator function.

--- Global/alias.sh
+++ Global/alias.sh
@@ -7,3 +7,8 @@ alias +x="chmod +x $1"
 # Set the tab title in iTerm 2
 function tt { echo -ne "\033];${1}\007"; }
 
+genpasswd() {
+	local l=$1
+       	[ "$l" == "" ] && l=20
+      	tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs
+}
