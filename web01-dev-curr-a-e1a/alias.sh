83af5b044a4f5247b43c695e7c1b5530f6dae993
Jared McGuire <jrdmcgr@gmail.com>
2012-06-08 08:34:53 -0400

    Another MySQL function.

--- web01-dev-curr-a-e1a/alias.sh
+++ web01-dev-curr-a-e1a/alias.sh
@@ -5,3 +5,4 @@ alias pykill='pkill -u jmcguire python'
 tablegrep() { mysql jmcguire -e "show tables" | egrep ${1}; }
 trunctablegrep() { for i in `tablegrep ${1}`; do mysql jmcguire -e "truncate table $i"; done; }
 tablediff() { diff -u <(mysql ${1} -e 'show tables') <(mysql ${2} -e 'show tables') | vi -; }
+selectstar() { for args; do mysql jmcguire -e "select * from $args"; done; }
