78c53ed8ba7b7115e182847cbe024ef33bf0953a
Jared McGuire <jrdmcgr@gmail.com>
2012-07-10 11:56:44 -0400

    Added vimrc, etc.

--- Titan.local/env.sh
+++ Titan.local/env.sh
@@ -3,4 +3,3 @@ export EC2_HOME=~/.ec2
 export EC2_PRIVATE_KEY=`ls $EC2_HOME/pk-*.pem`
 export EC2_CERT=`ls $EC2_HOME/cert-*.pem`
 export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home/
-
