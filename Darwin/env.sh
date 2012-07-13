8db2492059e9c405975dc7dcb15df817c55d7e23
Jared McGuire <jrdmcgr@gmail.com>
2012-07-13 11:27:30 -0400

    Changes to titan.

--- Darwin/env.sh
+++ Darwin/env.sh
@@ -5,8 +5,7 @@ export GEM_HOME='/usr/local/Cellar/gems/1.8'
 export GIT_EDITOR='subl -w'
 export GITHUB_USER='jrdmcgr'
 
+
 # This is specifically for the github Sublime Text plugin.
 # It exports environment vars to GUI applications.
 launchctl setenv PATH $PATH
-launchctl setenv GITHUB_USER $GITHUB_USER
-launchctl setenv GITHUB_TOKEN $GITHUB_TOKEN
