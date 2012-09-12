933a72818d8cc2daef6c41670df89801aebfb27d
Jared McGuire <jrdmcgr@gmail.com>
2012-09-12 14:48:13 -0400

    asdf

--- PS1.sh
+++ PS1.sh
@@ -1,7 +1,19 @@
+# regular colors
+K="\[\e[0;30m\]"    # black
+R="\[\e[0;31m\]"    # red
+G="\[\e[0;32m\]"    # green
+Y="\[\e[0;33m\]"    # yellow
+B="\[\e[0;34m\]"    # blue
+M="\[\e[0;35m\]"    # magenta
+C="\[\e[0;36m\]"    # cyan
+W="\[\e[0;37m\]"    # white
+X="\[\e[0m\]"
+
+
 home_prompt() {
 	# If we're home then use a simple prompt.
 	if [ `pwd` == $HOME ]; then 
-		PS1=' \[\e[0;32m\]⚡\[\e[0m\] '
+		PS1=$G' ⚡ '$X
 	# Check for git bash completion on Mac.
 	elif [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]; then
 		. /usr/local/etc/bash_completion.d/git-completion.bash;
