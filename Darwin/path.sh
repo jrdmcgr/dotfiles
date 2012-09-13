1e3084f94938de801114df0ebd57a4b65aca788e
Jared McGuire <jrdmcgr@gmail.com>
2012-09-13 08:45:32 -0400

    Fixed gem path.

--- Darwin/path.sh
+++ Darwin/path.sh
@@ -1,6 +1,7 @@
+PATH=/usr/local/bin
 PATH=$PATH:/usr/local/share/python/
-PATH=$PATH:/usr/local/Cellar/gems/*/bin/
-PATH=/usr/local/bin:$PATH
+PATH=$PATH:/usr/local/Cellar/gems/1.8/bin/
+PATH=$PATH:/usr/bin:/usr/sbin:/bin:/sbin
 
 # This is specifically for the github Sublime Text plugin.
 # It exports environment vars to GUI applications.
