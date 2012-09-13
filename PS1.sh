833aab9d07eabd30d48642d0c323e2b310ecf62e
Jared McGuire <jrdmcgr@gmail.com>
2012-09-12 21:58:54 -0400

    Colors.

--- PS1.sh
+++ PS1.sh
@@ -19,3 +19,21 @@ home_prompt() {
 PROMPT_COMMAND=home_prompt
 
 
+
+local         BLACK="\[\033[0;30m\]"
+local           RED="\[\033[0;31m\]"
+local         GREEN="\[\033[0;32m\]"
+local        YELLOW="\[\033[0;33m\]"
+local          BLUE="\[\033[0;34m\]"
+local       MAGENTA="\[\033[0;35m\]"
+local          CYAN="\[\033[0;36m\]"
+local    LIGHT_GRAY="\[\033[0;37m\]"
+local     LIGHT_RED="\[\033[1;31m\]"
+local   LIGHT_GREEN="\[\033[1;32m\]"
+local  LIGHT_YELLOW="\[\033[1;33m\]"
+local    LIGHT_BLUE="\[\033[1;34m\]"
+local LIGHT_MAGENTA="\[\033[1;35m\]"
+local    LIGHT_CYAN="\[\033[1;36m\]"
+local         WHITE="\[\033[1;37m\]"
+local    COLOR_NONE="\[\e[0m\]"
+
