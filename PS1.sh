6ed42cc2548ade6edd1e7c7e0589e539901fbdf3
Jared McGuire <jrdmcgr@gmail.com>
2012-11-15 16:54:04 -0500

    I guess __git_prompt stuff got moved into it's own bash completion script.

--- PS1.sh
+++ PS1.sh
@@ -18,8 +18,13 @@ home_prompt() {
 	# If we're home then use a simple prompt.
 	if [ `pwd` == $HOME ]; then 
 		PS1=' \[\e[0;32m\]⚡\[\e[0m\] '
-	elif [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]; then
+
+    # Check for git bash completion on Mac.
+    elif [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]; then
 		. /usr/local/etc/bash_completion.d/git-completion.bash;
+        if [ -f /usr/local/etc/bash_completion.d/git-prompt.sh ]; then
+            . /usr/local/etc/bash_completion.d/git-prompt.sh
+        fi
 		PS1='〈 \w$(__git_ps1 " ± %s") 〉\[\e[0;32m\]⚡\[\e[0m\]  '
 
 	# Check for git bash completion on Linux.	
@@ -35,22 +40,4 @@ home_prompt() {
 
 PROMPT_COMMAND=home_prompt
 
-
-
-local         BLACK="\[\033[0;30m\]"
-local           RED="\[\033[0;31m\]"
-local         GREEN="\[\033[0;32m\]"
-local        YELLOW="\[\033[0;33m\]"
-local          BLUE="\[\033[0;34m\]"
-local       MAGENTA="\[\033[0;35m\]"
-local          CYAN="\[\033[0;36m\]"
-local    LIGHT_GRAY="\[\033[0;37m\]"
-local     LIGHT_RED="\[\033[1;31m\]"
-local   LIGHT_GREEN="\[\033[1;32m\]"
-local  LIGHT_YELLOW="\[\033[1;33m\]"
-local    LIGHT_BLUE="\[\033[1;34m\]"
-local LIGHT_MAGENTA="\[\033[1;35m\]"
-local    LIGHT_CYAN="\[\033[1;36m\]"
-local         WHITE="\[\033[1;37m\]"
-local    COLOR_NONE="\[\e[0m\]"
 
