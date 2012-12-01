86d81c3f67961c0f2adc184e37c59eaea2f2480b
Jared McGuire <jrdmcgr@gmail.com>
2012-12-01 09:46:44 -0500

    Removed trailing slashes. Added heroku bin.

--- Darwin/path.sh
+++ Darwin/path.sh
@@ -1,11 +1,13 @@
 PATH=$HOME/bin
 PATH=$PATH:/usr/local/bin:/usr/local/sbin
-PATH=$PATH:/usr/local/share/python/
-PATH=$PATH:/usr/local/Cellar/gems/1.8/bin/
+PATH=$PATH:/usr/local/share/python
+PATH=$PATH:/usr/local/Cellar/gems/1.8/bin
 PATH=$PATH:/usr/bin:/usr/sbin:/bin:/sbin
 PATH=$PATH:/usr/local/Cellar/ruby/1.9.3-p286/bin
-# node modules
+# Node modules
 PATH=$PATH:/usr/local/share/npm/bin
+# Heroku
+PATH=$PATH:/usr/local/heroku/bin
 
 export PATH
 export PYTHONPATH='/usr/local/lib/python2.7/site-packages'
