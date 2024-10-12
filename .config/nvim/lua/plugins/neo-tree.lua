bf15e2f0f405083a7b47bd2b3eca7f2124aabe3a
Jared McGuire <jrdmcgr@gmail.com>
2024-10-12 14:09:08 -0400

    nvim - move file tree to right

---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        opts = {
+            window = {
+                position = "right",  -- This sets the explorer to the right side
+            }
+        }
+    }
+}
