39808345232ca2af1c715031bc0ea6d76e2a58a9
Jared McGuire <jrdmcgr@gmail.com>
2025-05-28 23:57:46 -0400

    Updates to scripts

---
+++ bin/mk-repo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+if [ -z "$1" ]; then
+  echo "Usage: mk-repo REPO_NAME"
+  exit 1
+fi
+gh repo create "$1" \
+  --private \
+  --add-readme \
+  --disable-wiki \
+  --disable-issues \
+  --clone
